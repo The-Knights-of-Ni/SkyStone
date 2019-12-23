4a0e3bd651c3d6101c28a550edf5ceaf4ea8a04f
Overlord999 <32445772+Overlord999@users.noreply.github.com>
2019-12-23 11:39:48 -0800

    Update Drive.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drive.java
@@ -14,6 +14,15 @@ public class Drive extends Subsystem {
     public DcMotorEx rearLeft;
     public DcMotorEx rearRight;
 
+    //DO WITH ENCODERS
+    private static final double     COUNTS_PER_MOTOR_REV    = 537.6*0.646;    // AM Orbital 20 motor
+    private static final double     DRIVE_GEAR_REDUCTION    = 1.0 ;     // This is < 1.0 if geared UP
+    private static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
+    private static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * 3.1415);
+    private static final double     COUNTS_CORRECTION_X     = 0.939;
+    private static final double     COUNTS_CORRECTION_Y     = 0.646;
+
     //Sensors
     private BNO055IMU imu;
 
@@ -30,6 +39,7 @@ public class Drive extends Subsystem {
     /**
      * Stops all drive motors
      */
+    /** Stops all drive motors */
     public void stop() {
         frontLeft.setPower(0);
         frontRight.setPower(0);
@@ -37,9 +47,7 @@ public class Drive extends Subsystem {
         rearRight.setPower(0);
     }
 
-    /**
-     * Sets all drive motors to specified run mode
-     */
+    /** Sets all drive motors to specified run mode */
     public void setRunMode(DcMotor.RunMode mode) {
         frontLeft.setMode(mode);
         frontRight.setMode(mode);
@@ -47,9 +55,7 @@ public class Drive extends Subsystem {
         rearRight.setMode(mode);
     }
 
-    /**
-     * Sets all drive motors to specified zero power behavior
-     */
+    /** Sets all drive motors to specified zero power behavior */
     public void setZeroPowerBehavior(DcMotor.ZeroPowerBehavior mode) {
         frontLeft.setZeroPowerBehavior(mode);
         frontRight.setZeroPowerBehavior(mode);
@@ -78,6 +84,87 @@ public class Drive extends Subsystem {
         rearRight.setTargetPosition(targetPosition);
     }
 
+    public double getYaw() {
+        return imu.getAngularOrientation().firstAngle;
+    }
+
+    public void turnByAngle(double power, double turnAngle) {
+        double initialAngle = getYaw();
+        setRunMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        setRunMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        if (turnAngle > 0.0) {
+            // counter-clockwise
+            double currentAngle = initialAngle;
+            while (Math.abs(currentAngle - initialAngle - turnAngle) > 2) {
+                turn(-power);
+                currentAngle = getYaw();
+                if (currentAngle < initialAngle) {
+                    // angle wraparound
+                    currentAngle += 360.0;
+                }
+            }
+        } else {
+            // clockwise
+            double currentAngle = initialAngle;
+            while (Math.abs(currentAngle - initialAngle - turnAngle) > 2) {
+                turn(power);
+                currentAngle = getYaw();
+                if (currentAngle > initialAngle) {
+                    // angle wraparound
+                    currentAngle -= 360.0;
+                }
+            }
+        }
+        stop();
+    }
+
+    public void moveToPos2D(double power, double targetPositionX, double targetPositionY){
+        // move to X, Y position relative to the robot coordinate system
+        // the center of robot is 0,0
+        setRunMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        setRunMode(DcMotor.RunMode.RUN_TO_POSITION);
+        // convert from inches to motor counts
+        // correct for X and Y motion asymmetry
+        double distanceCountX, distanceCountY;
+        distanceCountX = targetPositionX * COUNTS_PER_INCH * COUNTS_CORRECTION_X;
+        distanceCountY = targetPositionY * COUNTS_PER_INCH * COUNTS_CORRECTION_Y;
+        setTargetPosition2D(distanceCountX, distanceCountY);
+        setPower2D(distanceCountX, distanceCountY, power);
+        while (frontLeft.isBusy() && frontRight.isBusy() && rearLeft.isBusy() && rearRight.isBusy()) {
+
+        }
+        stop();
+    }
+
+    private void setPower2D(double targetPositionX, double targetPositionY, double motorPower) {
+        // distribute power appropriately according to the direction of motion
+        double[] motorPowers = calcMotorPowers2D(targetPositionX, targetPositionY, motorPower);
+        rearLeft.setPower(motorPowers[0]);
+        frontLeft.setPower(motorPowers[1]);
+        rearRight.setPower(motorPowers[2]);
+        frontRight.setPower(motorPowers[3]);
+    }
+
+    private void setTargetPosition2D(double targetPositionX, double targetPositionY) {
+        // set motor rotation targets appropriately according to the direction of motion
+        frontLeft.setTargetPosition((int)  ((+ targetPositionX + targetPositionY)*Math.sqrt(2.0)));
+        frontRight.setTargetPosition((int) ((- targetPositionX + targetPositionY)*Math.sqrt(2.0)));
+        rearLeft.setTargetPosition((int)   ((- targetPositionX + targetPositionY)*Math.sqrt(2.0)));
+        rearRight.setTargetPosition((int)  ((+ targetPositionX + targetPositionY)*Math.sqrt(2.0)));
+    }
+
+    private double[] calcMotorPowers2D(double targetPositionX, double targetPositionY, double motorPower)
+    {
+        // targetPositionX and targetPositionY determine the direction of movement
+        // motorPower determines the magnitude of motor power
+        double angleScale = Math.abs(targetPositionX) + Math.abs(targetPositionY);
+        double lrPower = motorPower * (- targetPositionX + targetPositionY) / angleScale;
+        double lfPower = motorPower * (+ targetPositionX + targetPositionY) / angleScale;
+        double rrPower = motorPower * (+ targetPositionX + targetPositionY) / angleScale;
+        double rfPower = motorPower * (- targetPositionX + targetPositionY) / angleScale;
+        return new double[]{lrPower, lfPower, rrPower, rfPower};
+    }
+
     /**
      * Positive encoder values correspond to rightward robot movement
      */
@@ -88,7 +175,4 @@ public class Drive extends Subsystem {
         rearRight.setTargetPosition(targetPosition);
     }
 
-    public double getYaw() {
-        return imu.getAngularOrientation().firstAngle;
-    }
 }
